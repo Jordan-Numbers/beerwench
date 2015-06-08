4bd8bbadbf16a231dd15f47ed9b17637756123e3
trosane <trimusicak@gmail.com>
2015-06-07 17:12:33 -0700

    list adapter and beer_list_layout

---
+++ app/src/main/java/edu/washington/beerswains/beerwench/Beer.java
@@ -0,0 +1,34 @@
+package edu.washington.beerswains.beerwench;
+
+/**
+ * Created by Trent on 6/7/15.
+ */
+public class Beer {
+    private String name;
+    private String abv;
+    private String producer;
+    private String pictureUrl;
+    private String id;
+    private String description;
+
+    public Beer(String name, String abv, String producer, String pictureUrl, String id, String description) {
+        this.name = name;
+        this.abv = abv;
+        this.producer = producer;
+        this.pictureUrl = pictureUrl;
+        this.id = id;
+        this.description = description;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getAbv() {
+        return this.abv;
+    }
+
+    public String getProducer() {
+        return this.producer;
+    }
+}
